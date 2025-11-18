2a7095f7912e61bb0cfda83d2ecb001fed578472
Shappy99 <142829878+Shappy99@users.noreply.github.com>
2025-11-18 12:22:48 +0200

    cursorHighlight

--- Scripts/world_gen.gd
+++ Scripts/world_gen.gd
@@ -1,6 +1,8 @@
 class_name worldGenerator
 extends TileMapLayer
 
+@onready var marker: Node2D = get_node("Marker") # Child Node2D
+
 @export var map_width : int = 40
 @export var map_height : int = 30
 @export var map_res_mult : float = 10.0
@@ -35,4 +37,25 @@ func _physics_process(_delta):
 	var mouse_pos_global = get_viewport().get_mouse_position()
 	var mouse_pos_local = to_local(mouse_pos_global)
 	var tile_pos = local_to_map(mouse_pos_local)
-	print(tile_pos)
+	#print(tile_pos)
+	print (get_is_interactable(tile_pos))
+	if (get_is_interactable(tile_pos)):
+		highlight_hex(tile_pos)
+	else:
+		highlight_hex(Vector2i(-2,-2))
+
+func highlight_hex(cellPos: Vector2i):
+	marker.position = map_to_local(cellPos)
+
+func get_is_interactable(tile_pos) -> bool:
+	var tilemap: TileMapLayer = get_tree().get_first_node_in_group("tilemap")
+	var cell = tile_pos
+	var data: TileData = tilemap.get_cell_tile_data(cell)
+	
+	if data:
+		var is_interactable: float = data.get_custom_data("interactable")
+		if is_interactable == 1:
+			print("1")
+			return true
+	print("0")
+	return false
