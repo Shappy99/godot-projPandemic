69a965161f45f82eefa304ae3c372823b5411440
Shappy99 <142829878+Shappy99@users.noreply.github.com>
2025-11-22 22:12:54 +0200

    bigUpdate

--- Scripts/world_gen.gd
+++ Scripts/world_gen.gd
@@ -9,11 +9,30 @@ extends TileMapLayer
 
 @export var map_height_map : FastNoiseLite = null
 
+func is_equal_to_0(number):
+	return number == 0
+func is_equal_to_1(number):
+	return number == 1
+func is_equal_to_2(number):
+	return number == 2
+func isSeedOk():
+	if mapArray.filter(is_equal_to_2).size() >= 50 && mapArray.filter(is_equal_to_2).size() <= 100: #200-250
+		if mapArray.filter(is_equal_to_1).size() >= 250 && mapArray.filter(is_equal_to_1).size() <= 300: #100-150
+			return true
+	else: return false
+
+var mapArray = []
+
 func generate_map() -> void:
 	var tile_map_tile_count : int = tile_set.get_source(0).get_tiles_count()-1
 	
 	clean_terrain_map()
+	$"../forestLayer".clear()
+	$"../forest2Layer".clear()
+	$"../forest3Layer".clear()
+	$"../peaksLayer".clear()
 	set_up_map_height_map()
+	mapArray.clear()
 	
 	for x in map_width:
 		for y in map_height:
@@ -22,9 +41,40 @@ func generate_map() -> void:
 			if random_height_value > tile_map_tile_count:
 				random_height_value = tile_map_tile_count
 			
-			print(str(random_height_value))
+			mapArray.append(random_height_value)
 			
-			set_cell (Vector2i(x-1,y-1), 0, Vector2i(random_height_value,0), 0)
+			$".".set_cell (Vector2i(x,y), 1, Vector2i(random_height_value,0), 0)
+			if (random_height_value==1):
+				randomize()
+				if randi()%2==0:
+					if randi()%2==1:
+						$"../forestLayer".set_cell (Vector2i(x,y), 0, Vector2i(0,0), 0)
+				randomize()
+				if randi()%2==1:
+					if randi()%2==0:
+						$"../forest2Layer".set_cell (Vector2i(x,y), 0, Vector2i(0,0), 0)
+				randomize()
+				if randi()%2==0:
+					if randi()%2==1:
+						$"../forest3Layer".set_cell (Vector2i(x,y), 0, Vector2i(0,0), 0)
+			elif (random_height_value==2):
+				randomize()
+				var randomNumber = randi()%10
+				if (randomNumber)==5:
+					var numberOfTiles = (randi()%4)+1
+					while numberOfTiles!=0:
+						var tile_pos = local_to_map(Vector2i(x,y))
+						if (!get_is_peak(tile_pos)):
+							$"../peaksLayer".set_cell (Vector2i(x,y), 0, Vector2i(0,0), 0)
+							numberOfTiles-=1
+						else:
+							print("Already peak")
+							numberOfTiles=0
+						print("Tiles", numberOfTiles)
+	if !isSeedOk():
+		generate_map()
+	else:
+		print(mapArray.size())
 
 func set_up_map_height_map() -> void:
 	randomize()
@@ -38,13 +88,13 @@ func _physics_process(_delta):
 	var mouse_pos_local = to_local(mouse_pos_global)
 	var tile_pos = local_to_map(mouse_pos_local)
 	#print(tile_pos)
-	print (get_is_interactable(tile_pos))
+	#print (get_is_interactable(tile_pos))
 	if (get_is_interactable(tile_pos)):
 		highlight_hex(tile_pos)
-		print ("highlight")
+		#print ("highlight")
 	else:
 		highlight_hex(Vector2i(-2,-2))
-		print ("not highlight")
+		#print ("not highlight")
 
 func highlight_hex(cellPos: Vector2i):
 	marker.position = map_to_local(cellPos)
@@ -57,7 +107,20 @@ func get_is_interactable(tile_pos) -> bool:
 	if data:
 		var is_interactable: float = data.get_custom_data("interactable")
 		if is_interactable == 1:
-			print("1")
+			#print("1")
 			return true
-	print("0")
+	#print("0")
+	return false
+
+func get_is_peak(tile_pos) -> bool:
+	var tilemap: TileMapLayer = get_tree().get_first_node_in_group("tilepeak")
+	var cell = tile_pos
+	var data: TileData = tilemap.get_cell_tile_data(cell)
+	
+	if data:
+		var is_peak: float = data.get_custom_data("isPeak")
+		if is_peak == 1:
+			#print("1")
+			return true
+	#print("0")
 	return false
