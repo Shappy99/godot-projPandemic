48b72eaa475f22ca9341d78206269bc659f7ba7a
Anghelescu Florin <florin-alexandru.anghelescu@s.unibuc.ro>
2025-11-26 17:29:11 +0200

    more updates on the research tree

--- Scripts/UI/research/skill_button.gd
+++ Scripts/UI/research/skill_button.gd
@@ -4,23 +4,41 @@ class_name SkillNode
 @onready var panel: Panel = $Panel
 @onready var label: Label = $MarginContainer/Label
 @onready var line_2d: Line2D = $Line2D
-
+@export var root = [0]
+@export var unlocked = false
+@onready var list = get_parent().get_children()
 
 const skillButtonIcon := preload("res://skill_placeholder.png")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	texture_rect.set_texture(skillButtonIcon)
-	if get_parent() is SkillNode:
-		line_2d.add_point(global_position + size/2)
-		line_2d.add_point(get_parent().global_position + size/2)
+	for i in range(list.size()):
+		if i in root:
+			print (i,"radacina nod")
+			line_2d.add_point(global_position + size/2)
+			line_2d.add_point(list[i].global_position + size/2)
 
-var level: int = 0:
-	set(value):
-		level = value
-		label.text = str(level) + "/3"
+#var level: int = 0:
+	#set(value):
+		#level = value
+		#label.text = str(level) + "/3"
 
 
 func _on_pressed() -> void:
-	level = min(level+1,3)
-	panel.show_behind_parent = true
+	#level = min(level+1,3)
 	
+	panel.show_behind_parent = true
+	line_2d.default_color = Color(1.0, 0.0, 0.0, 1.0)
+	unlocked = true
+	print (unlocked)
+	print (get_index())
+	for skill in list:
+		if get_index() in skill.root:
+			print (skill.root)
+			var all_unlocked = true
+			for r in skill.root:
+				if not list[r].unlocked:
+					all_unlocked = false
+					break
+			if all_unlocked:
+				skill.disabled = false
